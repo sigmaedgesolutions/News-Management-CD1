948779d61408689849f3a18057eb498e67cac87e
sigma edge <sigmaedgesolutions@gmail.com>
2026-03-16 17:19:07 +0530

    Working on admin dashboard

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,3 +1,6 @@
 export default function AdminDashboard() {
-  return <h2 className="p-4 text-xl font-bold">Admin Dashboard</h2>;
+  return <div>
+    <h2 className="p-4 text-xl font-bold">Admin Dashboard</h2>;
+    <h2 className="p-4 text-xl font-bold">I am working here</h2>;
+    </div>
 }
