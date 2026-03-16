4f324cde735617a0f7f3ac9079b23813c981a7f3
sigma edge <sigmaedgesolutions@gmail.com>
2026-03-16 16:31:02 +0530

    header logo changed as bold text

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,13 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center p-4 border-b bg-white">
       {/* Logo */}
-      <div className="flex items-center">
-        
-        <h3>Todays News</h3>
-      </div>
+    {/* Logo */}
+<div className="flex items-center">
+  <h3 className="text-2xl font-extrabold tracking-wide text-blue-700">
+    Todays News
+  </h3>
+</div>
+
 
       {/* Ad Banner */}
       <div className="w-[300px] h-[60px] bg-gray-200 flex items-center justify-center">
